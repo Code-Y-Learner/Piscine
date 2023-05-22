7770bcaf70684dbb88eeb2a95cef7dad241b6708
Code.Y.Learner <jktmdwo@naver.com>
2023-05-22 20:04:03 +0900

    Add files via upload

---
+++ c09/ex01/srcs/ft_strcmp.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seungjyu <seungjyu@student.24seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 22:10:29 by seungjyu          #+#    #+#             */
+/*   Updated: 2023/02/16 02:50:22 by seungjyu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strcmp(char *s1, char *s2);
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (!(s1[i] == '\0' && s2[i] == '\0'))
+	{
+		if (s1[i] == s2[i])
+			i++;
+		else
+			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
+	}
+	return (0);
+}
